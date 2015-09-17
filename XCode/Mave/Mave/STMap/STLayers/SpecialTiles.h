7f04e0f05d32e7dd7723c783c3adc288b4e1cf44
KayhanQ <kayhanfqaiser@gmail.com>
2015-09-16 23:16:46 -0400

    New Levels

---
+++ XCode/Mave/Mave/STMap/STLayers/SpecialTiles.h
@@ -0,0 +1,17 @@
+//
+//  LevelInfo.h
+//  Mave
+//
+//  Created by Kayhan Qaiser on 2015-09-16.
+//  Copyright (c) 2015 Kayhan Qaiser. All rights reserved.
+//
+
+@interface SpecialTiles : NSObject
+{
+    
+}
+
+- (id)initWithFilename:(NSString*)filepath;
+
+
+@end
---
+++ XCode/Mave/Mave/STMap/STLayers/SpecialTiles.m
@@ -0,0 +1,26 @@
+//
+//  LevelInfo.m
+//  Mave
+//
+//  Created by Kayhan Qaiser on 2015-09-16.
+//  Copyright (c) 2015 Kayhan Qaiser. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "SpecialTiles.h"
+#import "TBXML.h"
+#import "NPC.h"
+
+@implementation SpecialTiles
+{
+    
+}
+
+- (id)initWithFilename:(NSString *)filepath {
+    if (self = [super init]) {
+        
+    }
+    return self;
+}
+
+@end
